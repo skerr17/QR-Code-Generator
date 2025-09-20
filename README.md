d0bd0b7c531d9ff6dcf9e42dfd0587353be66bfc
Stephen Kerr <kerrstephen17@gmail.com>
2025-09-20 00:19:03 +0000

    added in the code and created some qrcodes

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # QR-Code-Generator
 In this Repo I want to put the code needed to create a QR code from a URL
+
+Following this tutorial
+- https://realpython.com/python-generate-qr-code/
--- basic_qr_code.py
+++ basic_qr_code.py
@@ -1,3 +1,34 @@
 # in this script any url can be used to create a basic qr code,
-# using Segno library 
+# using Segno library Seedocumentation -> https://segno.readthedocs.io/en/latest/ 
 # Created by: Stephen Kerr - 20/09/25
+
+
+# basic_qrcode.py
+
+import segno
+
+qrcode = segno.make_qr("https://segno.readthedocs.io/en/latest/")
+
+qrcode_rotated = qrcode.to_pil().rotate(45)
+
+qrcode_rotated.save(
+    "rotated_qrcode.png",
+    scale=5,
+    border= 4,#
+    light = "white",
+    dark = "darkblue",
+    quiet_zone = "black",
+    data_dark = "pink",
+    data_light = "green" 
+    )
+
+qrcode.save(
+    "basic_qrcode.png",
+    scale=5,
+    border= 4,#
+    light = "white",
+    dark = "darkblue",
+    quiet_zone = "black",
+    data_dark = "pink",
+    data_light = "green"
+    )
